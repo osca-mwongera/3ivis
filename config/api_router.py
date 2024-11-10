c368f701e98237a0e2d475d1bb05c9b0125f6bff
Osca Mwongera <oscamwongera@gmail.com>
2024-11-10 13:32:45 +0100

    Add data endpoint to api router

--- config/api_router.py
+++ config/api_router.py
@@ -1,13 +1,18 @@
 from django.conf import settings
+from django.urls import path
 from rest_framework.routers import DefaultRouter
 from rest_framework.routers import SimpleRouter
 
+from ivis.chart.api.views import DataView
 from ivis.users.api.views import UserViewSet
 
 router = DefaultRouter() if settings.DEBUG else SimpleRouter()
 
 router.register("users", UserViewSet)
 
-
 app_name = "api"
 urlpatterns = router.urls
+
+urlpatterns += [
+    path("data", DataView.as_view(), name="chart_data"),
+]
